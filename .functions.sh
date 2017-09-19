99e0505f5080cf3e3d9b0ff1351274575f60f3f6
Matt Rafferty <mjrafferty0@gmail.com>
2017-09-19 15:35:40 -0400

    updates

--- .functions.sh
+++ .functions.sh
@@ -504,8 +504,7 @@ sysusage () {
   echo;
   printf "%-10s %10s %10s %10s %10s\n%s\n" "User" "Mem (MB)" "Process" "CPU(%)" "MEM(%)" "$(dashes 54)";
   ps aux \
-    | grep -v "^USER" \
-    | awk '{ mem[$1]+=$6; procs[$1]+=1; pcpu[$1]+=$3; pmem[$1]+=$4; } END { for (i in mem) { printf "%-10s %10.2f %10d %9.1f%% %9.1f%%\n", i, mem[i]/(1024), procs[i], pcpu[i], pmem[i] } }' \
+    | awk ' !/^USER/ { mem[$1]+=$6; procs[$1]+=1; pcpu[$1]+=$3; pmem[$1]+=$4; } END { for (i in mem) { printf "%-10s %10.2f %10d %9.1f%% %9.1f%%\n", i, mem[i]/(1024), procs[i], pcpu[i], pmem[i] } }' \
     | sort -nrk$colsort \
     | head;
   echo;
