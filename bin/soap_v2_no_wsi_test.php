156e47980c7968e9416dfbc3cc43c917d0f8cbae
Matt Rafferty <mjrafferty0@gmail.com>
2017-11-01 19:26:44 -0400

    updates

--- bin/soap_v2_no_wsi_test.php
+++ bin/soap_v2_no_wsi_test.php
@@ -1,21 +1,20 @@
 <?php
 try {
-  $client = new SoapClient('http://mage1.mjrafferty.org/api/v2_soap?wsdl=1', array('trace' => 1, 'connection_timeout' => 120));
+  $client = new SoapClient('http://'.$argv[1].'/api/v2_soap?wsdl=1', array('trace' => 1, 'connection_timeout' => 120));
 
-  $session = $client->login('mjraffer', 'MaximsPerilLoosedAscend2549');
+  $session = $client->login($argv[2], $argv[3]);
 
   echo $client->__getLastRequest();
   echo $client->__getLastResponse();
 
-  //$result = $client->call($session, 'product.list',"");
-  //$result = $client->catalogProductList($session, "");
-  $result = $client->catalogProductUpdate($session, '905', array(
-    'stock_data' => array(
-      'qty' => '20',
-      'is_in_stock' => '1',
-      'manage_stock' => '1',
-      'notify_stock_qty' => '5'
-    )));
+  $result = $client->catalogProductList($session);
+  //$result = $client->catalogProductUpdate($session, '905', array(
+    //'stock_data' => array(
+      //'qty' => '20',
+      //'is_in_stock' => '1',
+      //'manage_stock' => '1',
+      //'notify_stock_qty' => '5'
+    //)));
 
   var_dump($result);
 
--- bin/soap_v2_with_wsi_test.php
+++ bin/soap_v2_with_wsi_test.php
@@ -1,17 +1,13 @@
 <?php
 try {
-  $client = new SoapClient('http://mage1.mjrafferty.org/api/v2_soap/?wsdl', array('trace' => 1, 'connection_timeout' => 120));
+  $client = new SoapClient('http://'.$argv[1].'/api/v2_soap/?wsdl', array('trace' => 1, 'connection_timeout' => 120));
 
-	$sessionId = $client->login((object)array('username' => 'mjraffer', 'apiKey' => 'MaximsPerilLoosedAscend2549'));
+  $sessionId = $client->login((object)array('username' => $argv[2], 'apiKey' => $argv[3]));
 
   echo $client->__getLastRequest();
   echo $client->__getLastResponse();
 
-	$result = $client->catalogProductUpdate((object)array('sessionId' => $sessionId->result, 'productId' => '905',
-		'productData' => ((object)array(
-			'name' => 'Product name updated',
-			'status' => '1',
-		))));
+  $result = $client->catalogProductList((object)array('sessionId' => $sessionId->result, 'filters' => null));
 
 	var_dump($result->result);
 
