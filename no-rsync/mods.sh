e98ef7022788fd0b10b2ee6205ace32594899ce6
Matt Rafferty <mjrafferty0@gmail.com>
2018-05-21 01:46:12 -0400

    updates

--- no-rsync/mods.sh
+++ no-rsync/mods.sh
@@ -255,11 +255,11 @@ _makeJson () {
 
     mapfile -t plugins < <(cat "${merges[merge]}") || echo "${merges[merge]}"
 
-    printf '{"ignoredDependencies":[],"method":"Overrides","dateBuilt":"12\/30\/1899","masters":[],"filename":"%s.esp","pluginHashes":[],"bIgnoreNonContiguous":false,"files":[],"fails":[],"name":"%s","plugins":[' "$(echo "${merges[merge]}" | sed 's/ /\\ /g')" "$(echo "${merges[merge]}" | sed 's/ /\\ /g')";
+    printf '{"ignoredDependencies":[],"method":"Overrides","dateBuilt":"12\/30\/1899","masters":[],"filename":"%s.esp","pluginHashes":[],"bIgnoreNonContiguous":false,"files":[],"fails":[],"name":"%s","plugins":[' "${merges[merge]}" "${merges[merge]}";
 
     for ((plugin=0; plugin < ${#plugins[*]}; plugin++)); do
 
-      printf '"%s"' "$(echo "${plugins[plugin]}" | sed 's/ /\\ /g')";
+      printf '"%s"'"${plugins[plugin]}";
 
       if (( plugin != ( ${#plugins[@]} - 1 ) )); then
         printf ",";
