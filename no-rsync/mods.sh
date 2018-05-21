03a4b63bb37960cb834cd67110b6415f681fd14d
Matt Rafferty <mjrafferty0@gmail.com>
2018-05-21 12:14:47 -0400

    updates

--- no-rsync/mods.sh
+++ no-rsync/mods.sh
@@ -261,11 +261,11 @@ _makeJson () {
 
     mapfile -t plugins < <(cat "${merges[merge]}") || echo "${merges[merge]}"
 
-    printf '{"ignoredDependencies":[],"method":"Overrides","dateBuilt":"12\/30\/1899","masters":[],"filename":"%s.esp","pluginHashes":[],"bIgnoreNonContiguous":false,"files":[],"fails":[],"name":"%s","plugins":[' "${merges[merge]/.txt/}" "${merges[merge]/.esp.txt/}";
+    printf '{"ignoredDependencies":[],"method":"Overrides","dateBuilt":"12\/30\/1899","masters":[],"filename":"%s","pluginHashes":[],"bIgnoreNonContiguous":false,"files":[],"fails":[],"name":"%s","plugins":[' "${merges[merge]/.txt/}" "${merges[merge]/.es[m,p].txt/}";
 
     for ((plugin=0; plugin < ${#plugins[*]}; plugin++)); do
 
-      printf '"%s"'"${plugins[plugin]}";
+      printf '"%s"' "${plugins[plugin]}";
 
       if (( plugin != ( ${#plugins[@]} - 1 ) )); then
         printf ",";
