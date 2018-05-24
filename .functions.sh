8dba8d98586978b81c87c365ea71cb3f41b4e16b
Matt Rafferty <mjrafferty0@gmail.com>
2018-05-23 20:27:01 -0400

    updates

--- .functions.sh
+++ .functions.sh
@@ -119,7 +119,7 @@ cdlogs () {
   fi
 
   # Gather relevant domain information
-  vhosts=($($GREP -El "Server(Name|Alias).* $query" /etc/httpd/{conf.d/vhost_*.conf,tmpdomains.d/*.conf}));
+  vhosts=($($GREP -El "Server(Name|Alias).* $query" /etc/httpd/{conf.d/vhost_*.conf,tmpdomains.d/*.conf} 2> /dev/null));
 
   for (( i=ARRAY_START; i<${#vhosts[@]}+ARRAY_START; i++ )); do
     logsdir[$i]=$($GREP -Poh '[^#]*ErrorLog.* \K/([^/]+/)+' "${vhosts[$i]}" \
