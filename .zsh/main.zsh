a325f3144ef9f32173a9ad79b7fea3f34b869507
Matt Rafferty <mjrafferty0@gmail.com>
2019-07-25 01:03:45 -0400

    Rearrange

--- .zsh/main.zsh
+++ .zsh/main.zsh
@@ -7,8 +7,20 @@ if type tmux &> /dev/null && [[  -z $TMUX && -n $SSH_TTY ]]; then
   exit;
 fi
 
+[ -r "${HOME}/.zsh/plugins_core.zsh" ] \
+  && source "${HOME}/.zsh/plugins_core.zsh"
+
 [ -r "${HOME}/.zsh/env.zsh" ] \
   && source "${HOME}/.zsh/env.zsh"
+
+[ -r "${HOME}/.zsh/completion.zsh" ] \
+  && source "$HOME/.zsh/completion.zsh"
+
+[ -r "${HOME}/.zsh/alias.zsh" ] \
+  && source "${HOME}/.zsh/alias.zsh"
+
+[ -r "${HOME}/.zsh/functions.zsh" ] \
+  && source "${HOME}/.zsh/functions.zsh"
 
 ## cd options ##
 setopt autocd
@@ -26,49 +38,7 @@ setopt hist_fcntl_lock
 setopt hist_verify
 unsetopt beep
 
-#[ -r "${HOME}/.zsh/zsh-histdb/sqlite-history.zsh" ] \
-  #&& source "$HOME/.zsh/zsh-histdb/sqlite-history.zsh"
-
-[ -r "${HOME}/.zsh/apollo-zsh-theme/prompt_apollo_setup" ] \
-  && source "$HOME/.zsh/apollo-zsh-theme/prompt_apollo_setup"
-
-[ -r "${HOME}/.zsh/zsh-completionsu[]/zsh-completions.plugin.zsh" ] \
-  && source "$HOME/.zsh/zsh-completions/zsh-completions.plugin.zsh"
-
-#[ -r "${HOME}/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" ] \
-  #&& source "$HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
-
-if [ -r "${HOME}/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh" ]; then
-
-  source "$HOME/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh"
-
-  bindkey '^[[A' history-substring-search-up
-  bindkey '^[[B' history-substring-search-down
-
-  bindkey -M vicmd 'k' history-substring-search-up
-  bindkey -M vicmd 'j' history-substring-search-down
-
-fi
-
-[ -r "${HOME}/.zsh/completion.zsh" ] \
-  && source "$HOME/.zsh/completion.zsh"
-
-if [ -r "${HOME}/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
-
-  source "$HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-
-  ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets line)
-  ZSH_HIGHLIGHT_STYLES[line]='bold'
-
-fi
-
-[ -r "${HOME}/.zsh/alias.zsh" ] \
-  && source "${HOME}/.zsh/alias.zsh"
-
-[ -r "${HOME}/.zsh/functions.zsh" ] \
-  && source "${HOME}/.zsh/functions.zsh"
-
-fpath=($HOME/.zsh/completions $fpath)
+#fpath=($HOME/.zsh/completions $fpath)
 
 setopt nullglob
 
