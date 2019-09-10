b3cfaabb2b361f1893525e26110ed627313c001a
Matt Rafferty <mjrafferty0@gmail.com>
2019-09-09 23:07:31 -0400

    Changes

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -18,17 +18,19 @@ fi
 
 setopt complete_aliases
 setopt auto_name_dirs
-setopt rec_exact
 setopt list_types
+setopt glob_complete
+setopt hash_list_all
+setopt menu_complete
 
 setopt COMPLETE_IN_WORD    # Complete from both ends of a word.
 setopt ALWAYS_TO_END       # Move cursor to the end of a completed word.
 setopt PATH_DIRS           # Perform path search even on command names with slashes.
-setopt AUTO_MENU           # Show completion menu on a successive tab press.
 setopt AUTO_LIST           # Automatically list choices on ambiguous completion.
+setopt list_ambiguous
+setopt list_packed
 setopt AUTO_PARAM_SLASH    # If completed parameter is a directory, add a trailing slash.
 setopt EXTENDED_GLOB       # Needed for file modification glob modifiers with compinit
-unsetopt MENU_COMPLETE     # Do not autoselect the first completion entry.
 unsetopt FLOW_CONTROL      # Disable start/stop characters in shell editor.
 
 #
