41a6f6adb1c1a486c50d1557ad4a59773e42fe48
Matt Rafferty <mjrafferty0@gmail.com>
2020-03-12 18:16:02 -0400

    new stuff

--- .zsh/plugins_optional.zsh
+++ .zsh/plugins_optional.zsh
@@ -4,13 +4,15 @@ zload  RobSis/zsh-completion-generator      '0b'  '[[ -n ${ZLAST_COMMANDS[(r)gco
 zload  aperezdc/zsh-fzy                     '0c'  atload"bindkey '\ec' fzy-cd-widget; bindkey '^T' fzy-file-widget"
 zload  donnemartin/haxor-news               '0c'  as"program" atclone="pip install -e . ;pip install -r requirements-dev.txt" atpull"%atclone"
 zload  jhawthorn/fzy                        '0b'  as"program" if'[[ -z "$commands[fzy]" ]]' make"!PREFIX=$ZPFX install" atclone"cp contrib/fzy-* $ZPFX/bin/" pick"$ZPFX/bin/fzy*"
-zload  laggardkernel/git-ignore             '0c'  has'git'        pick'init.zsh'         atload'alias                 gi="git-ignore"'   blockf
+zload  k-vernooy/tetris                     '0c'  as"program" pick"bin/tetris" make
+zload  laggardkernel/git-ignore             '0c'  has'git' pick'init.zsh' atload'alias gi="git-ignore"' blockf
 #zload  micha/resty                          '1'
 zload  mptre/yank                           '0c'  as"program" pick"yank" make
 #zload  mvdan/sh                             '0c'  from"gh-r" as"program" mv"shfmt* -> shfmt"
 zload  seletskiy/zsh-fuzzy-search-and-edit  '0d'  atload"bindkey '^T' fzy-file-widget"
 zload  soimort/translate-shell              '0c'  if'[[ -n "$commands[gawk]" ]]'
 zload  tj/git-extras                        '0c'  as"program" pick"$ZPFX/bin/git-*" make"PREFIX=$ZPFX"
+zload  wustho/epr                           '0c'  as"program" mv"epr.py -> epr"
 zload  zdharma/z-p-submods                  '0a'
 zload  zdharma/zsh-diff-so-fancy            '0c'  as"program" pick"bin/git-dsf"
 #zsnip  OMZ::plugins/aws                     '0c'  svn
