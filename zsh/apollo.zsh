290af944e1a590730179cbf2774f52ccd02d2786
Matt Rafferty <mjrafferty0@gmail.com>
2020-04-07 22:22:47 -0400

    changes

--- zsh/apollo.zsh
+++ zsh/apollo.zsh
@@ -5,3 +5,6 @@ zstyle ':apollo:*:*:*:context:ssh' ignore_users ".*matt.*" ".*raff.*" "root"
 zstyle ':apollo:*:*:*:context:sudo' ignore_hosts ".*"
 
 zstyle ':apollo:*:*:*:git:*' ignore_submodules true
+
+zstyle ':apollo:*:*:*:dir:*' bookmark_patterns "/home/????*/*/html;/html"
+zstyle ':apollo:*:*:*:dir:*' bookmarks "apollo=$HOME/apollo-zsh-theme"
