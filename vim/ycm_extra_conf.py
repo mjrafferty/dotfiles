196fe347a720c9b20e8c40d0cc41737f025ad988
Matt Rafferty <mjrafferty0@gmail.com>
2020-08-13 17:24:52 -0400

    updates

--- vim/ycm_extra_conf.py
+++ vim/ycm_extra_conf.py
@@ -1,3 +1,4 @@
+
 import os
 import ycm_core
 
@@ -5,115 +6,67 @@ flags = [
     '-Wall',
     '-Wextra',
     '-Werror',
-    '-Wno-long-long',
-    '-Wno-variadic-macros',
     '-fexceptions',
     '-DNDEBUG',
-    '-DUSE_CLANG_COMPLETER',
     '-std=c++11',
     '-x',
     'c++',
     '-isystem',
-    '../BoostParts',
+    '/usr/include',
     '-isystem',
-    '/System/Library/Frameworks/Python.framework/Headers',
+    '/usr/local/include',
     '-isystem',
-    '../llvm/include',
+    '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/c++/v1',
     '-isystem',
-    '../llvm/tools/clang/include',
-    '-I',
-    '.',
-    '-I',
-    './ClangCompleter',
-    '-isystem',
-    './tests/gmock/gtest',
-    '-isystem',
-    './tests/gmock/gtest/include',
-    '-isystem',
-    './tests/gmock',
-    '-isystem',
-    './tests/gmock/include',
+    '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
 ]
 
 
 compilation_database_folder = ''
 
 if os.path.exists(compilation_database_folder):
-    database = ycm_core.CompilationDatabase(compilation_database_folder)
+  database = ycm_core.CompilationDatabase(compilation_database_folder)
 else:
-    database = None
+  database = None
 
 SOURCE_EXTENSIONS = ['.cpp', '.cxx', '.cc', '.c', '.m', '.mm']
 
 
 def DirectoryOfThisScript():
-    return os.path.dirname(os.path.abspath(__file__))
+  return os.path.dirname(os.path.abspath(__file__))
 
 
-def MakeRelativePathsInFlagsAbsolute(flags, working_directory):
-    if not working_directory:
-        return list(flags)
-  new_flags = []
-  make_next_absolute = False
-  path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
-  for flag in flags:
-      new_flag = flag
-
-    if make_next_absolute:
-        make_next_absolute = False
-        if not flag.startswith( '/' ):
-            new_flag = os.path.join( working_directory, flag )
-
-    for path_flag in path_flags:
-        if flag == path_flag:
-            make_next_absolute = True
-        break
-
-    if flag.startswith( path_flag ):
-        path = flag[ len( path_flag ): ]
-        new_flag = path_flag + os.path.join(working_directory, path)
-        break
-
-    if new_flag:
-        new_flags.append(new_flag)
-        return new_flags
+def IsHeaderFile(filename):
+  extension = os.path.splitext(filename)[1]
+  return extension in ['.h', '.hxx', '.hpp', '.hh']
 
 
-def IsHeaderFile( filename ):
-    extension = os.path.splitext( filename )[ 1 ]
-    return extension in [ '.h', '.hxx', '.hpp', '.hh' ]
-
-
-def GetCompilationInfoForFile( filename ):
-    if IsHeaderFile( filename ):
-        basename = os.path.splitext( filename )[ 0 ]
+def GetCompilationInfoForFile(filename):
+  if IsHeaderFile(filename):
+    basename = os.path.splitext(filename)[0]
     for extension in SOURCE_EXTENSIONS:
-        replacement_file = basename + extension
-        if os.path.exists( replacement_file ):
-            compilation_info = database.GetCompilationInfoForFile(
-                replacement_file )
+      replacement_file = basename + extension
+      if os.path.exists(replacement_file):
+        compilation_info = database.GetCompilationInfoForFile(
+          replacement_file)
         if compilation_info.compiler_flags_:
-            return compilation_info
+          return compilation_info
     return None
-return database.GetCompilationInfoForFile( filename )
+  return database.GetCompilationInfoForFile(filename)
 
 
-def FlagsForFile( filename, **kwargs ):
-    if database:
-        compilation_info = GetCompilationInfoForFile( filename )
-    if not compilation_info:
-        return None
+def FlagsForFile(filename, **kwargs):
+  if not database:
+    return {
+      'flags': flags,
+      'include_paths_relative_to_dir': DirectoryOfThisScript()
+    }
 
-    final_flags = MakeRelativePathsInFlagsAbsolute(
-        compilation_info.compiler_flags_,
-        compilation_info.compiler_working_dir_ )
+  compilation_info = GetCompilationInfoForFile(filename)
+  if not compilation_info:
+    return None
 
-    try:
-        final_flags.remove( '-stdlib=libc++' )
-    except ValueError:
-        pass
-  else:
-      relative_to = DirectoryOfThisScript()
-    final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
-
-    return { 'flags': final_flags }
+  return {
+    'flags': list(compilation_info.compiler_flags_),
+    'include_paths_relative_to_dir': compilation_info.compiler_working_dir_
+  }
