a7374a392ec87fe498b0b55b5196a96b96c769e6
Matt Rafferty <mjrafferty0@gmail.com>
2022-05-05 09:51:29 -0400

    TMUX - unique socket name, map config for liquidweb hosts

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -7,14 +7,15 @@ case "$HOST" in
   "mellon"*) TMUX_CONF="${XDG_CONFIG_HOME}/tmux/login_server.conf" ;;
   *".nexcess.net") TMUX_CONF="${XDG_CONFIG_HOME}/tmux/nexcess.conf" ;;
   *"nxcli.net") TMUX_CONF="${XDG_CONFIG_HOME}/tmux/nexcess.conf" ;;
+  *".liquidweb.com") TMUX_CONF="${XDG_CONFIG_HOME}/tmux/nexcess.conf" ;;
   "localhost") TMUX_CONF="${XDG_CONFIG_HOME}/tmux/default.conf" ;;
   *) TMUX_CONF="${XDG_CONFIG_HOME}/tmux/default.conf" ;;
 esac
 
 # Automatically open and close tmux session when connecting via SSH
 if type tmux &> /dev/null && [[  -z $TMUX && -n $SSH_TTY ]]; then
-  (tmux has-session -t "${HOME/*\//}" &> /dev/null && tmux attach -t "${HOME/*\//}") \
-    || tmux -f "${TMUX_CONF}" new-session -s "${HOME/*\//}"
+  (tmux -L "${HOME/*\//}" has-session -t "${HOME/*\//}" &> /dev/null && tmux -L "${HOME/*\//}" attach -t "${HOME/*\//}") \
+    || tmux -L "${HOME/*\//}" -f "${TMUX_CONF}" new-session -s "${HOME/*\//}"
       exit;
 fi
 
