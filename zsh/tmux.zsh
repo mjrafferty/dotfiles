a9e39e55dac4f89256b89d2d00abc2b22d7e07e0
Matt Rafferty <mjrafferty0@gmail.com>
2026-02-19 18:41:32 -0500

    Make tmux compatible with multiple versions

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -1,11 +1,19 @@
 # vim:ft=zsh
 
+[[ -e /etc/os-release ]] && OS="$(grep -Po 'PRETTY_NAME="\K[^"]*' /etc/os-release)"
+
 case "$HOST" in
-  "mellon"*)                  TMUX_CONF="${XDG_CONFIG_HOME}/tmux/login_server.conf" ;;
-  *".nexcess.net")            TMUX_CONF="${XDG_CONFIG_HOME}/tmux/nexcess.conf" ;;
-  *"nxcli.net")               TMUX_CONF="${XDG_CONFIG_HOME}/tmux/nexcess.conf" ;;
-  *".liquidweb.com")          TMUX_CONF="${XDG_CONFIG_HOME}/tmux/nexcess.conf" ;;
-  *)                          TMUX_CONF="${XDG_CONFIG_HOME}/tmux/default.conf" ;;
+  "mellon"*)
+    TMUX_CONF="${XDG_CONFIG_HOME}/tmux/login_server.conf" ;;
+  *".nexcess.net"|*"nxcli.net"|*".liquidweb.com")
+    case "${OS}" in
+      "Rocky Linux 9"*)
+        TMUX_CONF="${XDG_CONFIG_HOME}/tmux/nexcess.conf" ;;
+      *)
+        TMUX_CONF="${XDG_CONFIG_HOME}/tmux/nexcess_old.conf" ;;
+    esac ;;
+  *)
+    TMUX_CONF="${XDG_CONFIG_HOME}/tmux/default.conf" ;;
 esac
 
 # Automatically open and close tmux session when connecting via SSH
