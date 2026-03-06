184febf88b6cf330ce1654c638899af048825ad5
Matthew Rafferty <mjrafferty0@gmail.com>
2026-03-06 13:10:51 -0500

    Check for function before binding

--- zsh/bindings.zsh
+++ zsh/bindings.zsh
@@ -2,6 +2,14 @@
 
 bindkey -v
 
-bindkey -v '^?' backward-delete-char
-bindkey "^[[A" history-substring-search-up
-bindkey "^[[B"  history-substring-search-down
+(( ${+functions[backward-delete-char]} )) \
+  && bindkey -v '^?' backward-delete-char
+
+(( ${+functions[history-substring-search-up]} )) \
+  && bindkey "^[[A" history-substring-search-up
+
+(( ${+functions[history-substring-search-down]} )) \
+  && bindkey "^[[B"  history-substring-search-down
+
+(( ${+functions[__zhist_fzf]} )) \
+  && bindkey "^R" __zhist_fzf
